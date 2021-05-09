b1a7a06ae3e81d36de31f1f39c53235b331aa08f
Haji <a.haggi@yahoo.com>
2021-05-09 19:21:18 +0200

    2021-05-09 19:21:18,898

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <p>Operand1 in parent is {{ operand1 }}</p>
+  <button @click="resetOperand1">Reset</button>
+
+  <multiply :operand1="operand1" @multi-by="multiBy" />
+</template>
+
+<script >
+import { ref, computed } from "vue";
+import Multiply from "./components/Multiply.vue";
+
+export default {
+  components: {
+    Multiply,
+  },
+  setup() {
+    let operand1 = ref(1); // ref
+
+    const multiBy = (op) => {
+      console.log(op);
+      operand1.value *= op;
+    };
+
+    const resetOperand1 = () => {
+      operand1.value = 1;
+    };
+
+    return {
+      operand1,
+      multiBy,
+      resetOperand1
+    };
+  },
+};
+
+// import Multiply from "./components/Multiply.vue";
+
+// export default {
+//   components: {
+//     Multiply,
+//   },
+//   data() {
+//     return {
+//       operand1: 1,
+//     };
+//   },
+//   methods: {
+//     multiBy(op) {
+//       console.log(this.operand1);
+//       this.operand1 *= op;
+//     },
+//     resetOperand1() {
+//       this.operand1 = 1;
+//     },
+//   },
+// };
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: burlywood;
+}
+</style>
---
+++ src/components/Multiply.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>Child component</div>
+  <div>
+
+    (Recieved fm parent {{ operand1 }}) X
+    <input type="text" placeholder="Enter a num" v-model="operand2" />
+
+    <button @click.prevent="$emit('multi-by', operand2)">Multiply</button>
+  </div>
+</template>
+
+<script >
+import { ref } from "vue";
+export default {
+  props: {
+    operand1: Number,
+  },
+  emits: ["multi-by"],// look like angular @output
+
+  setup() {
+    const operand2 = ref(0);
+    return {
+      operand2,
+    };
+  },
+};
+
+// export default {
+//   props: {
+//     operand1: Number,
+//   },
+//     emits: ["multi-by"],// look like angular @output
+//   data() {
+//     const operand2 = 0;
+//     return {
+//       operand2,
+//     };
+//   },
+
+// };
+</script>
+
+<style scoped>
+div {
+  background-color: #97e8ee;
+}
+</style>
